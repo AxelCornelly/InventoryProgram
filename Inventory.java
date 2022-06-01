852558b8591289de76eea0ff6bcac3853571081d
AxelCornelly <ace.cornelly@gmail.com>
2022-06-01 15:37:31 -0700

    Create necessary files based on UML diagram.

---
+++ Inventory.java
@@ -0,0 +1,73 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ * Inventory class.
+ * Main application file
+ */
+
+/**
+ * @author Axel Cornelly
+ */
+
+import javafx.*;
+import javafx.collections.ObservableList;
+
+public class Inventory extends Application{
+    private static ObservableList<Part> allParts;
+    private static ObservableList<Product> allProducts;
+
+    public static void addPart(Part part) {
+
+    }
+
+    public static void addProduct(Product product) {
+
+    }
+
+    public static Part lookupPart(int partID) {
+
+    }
+
+    public static ObservableList<Part> lookupPart(String partName) {
+
+    }
+
+    public static Product lookupProduct(int productID) {
+
+    }
+
+    public static ObservableList<Product> lookupProduct(String productName) {
+
+    }
+
+    public static void updatePart(int index, Part selectedPart) {
+
+    }
+
+    public static void updateProduct(int index, Product newProduct) {
+
+    }
+
+    public static boolean deletePart(Part selectedPart) {
+
+    }
+
+    public static boolean deleteProduct(Product selectedProduct) {
+
+    }
+
+    public static ObservableList<Part> getAllParts() {
+
+    }
+
+    public static ObservableList<Product> getAllProducts() {
+
+    }
+
+    @Override
+    public void start(Stage appStage) {
+
+    }
+
+}
