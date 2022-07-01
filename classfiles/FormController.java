f6258531bc10da564740131f0d5e8fd40d68caad
AxelCornelly <ace.cornelly@gmail.com>
2022-07-01 13:59:05 -0700

    condensed ui files to address identical forms

--- classfiles/FormController.java
+++ classfiles/FormController.java
@@ -8,6 +8,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
@@ -30,19 +31,19 @@ public class FormController {
 
         switch(form) {
             case "addpartform":
-                page = (Pane) FXMLLoader.load(getClass().getResource("/UI/addpartform.fxml"));
+                page = (Pane) FXMLLoader.load(getClass().getResource("/UI/partform.fxml"));
                 stage.setScene(new Scene(page));
                 break;
             case "addproductform":
-                page = (Pane) FXMLLoader.load(getClass().getResource("/UI/addproductform.fxml"));
+                page = (Pane) FXMLLoader.load(getClass().getResource("/UI/productform.fxml"));
                 stage.setScene(new Scene(page));
                 break;
             case "modifypartform":
-                page = (Pane) FXMLLoader.load(getClass().getResource("/UI/modifypartform.fxml"));
+                page = (Pane) FXMLLoader.load(getClass().getResource("/UI/partform.fxml"));
                 stage.setScene(new Scene(page));
                 break;
             case "modifyproductform":
-                page = (Pane) FXMLLoader.load(getClass().getResource("/UI/modifyproductform.fxml"));
+                page = (Pane) FXMLLoader.load(getClass().getResource("/UI/productform.fxml"));
                 stage.setScene(new Scene(page));
                 break;
         }
--- classfiles/PartFormsController.java
+++ classfiles/PartFormsController.java
@@ -7,25 +7,28 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.RadioButton;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class PartFormsController {
     @FXML
-    private Button addPartSaveBtn, addPartCancelBtn, modifyPartSaveBtn, modifyPartCancelBtn;
+    private Button partSaveBtn, partCancelBtn;
 
     @FXML
-    public TextField modifyPartIDField, modifyPartNameField, modifyPartInvField, modifyPartPriceField, 
-                      modifyPartMaxField, modifyPartChangingField, modifyPartMinField, addPartIDField, 
-                      addPartNameField, addPartInvField, addPartPriceField, addPartMaxField, addPartChangingField, addPartMinField;
+    public TextField partIDField, partNameField, partInvField, partPriceField, 
+                      partMaxField, partChangingField, partMinField;
     
     @FXML
-    public RadioButton addPartInHouseRadioBtn, addPartOutsourcedRadioBtn, modifyPartInHouseRadioBtn, modifyPartOutsourcedRadioBtn;
+    public RadioButton inHouseRadioBtn, outsourcedRadioBtn;
 
+    @FXML
+    public Label partFormTitleLabel;
+    
     @FXML
     public void handleCancelBtn(ActionEvent e) throws IOException{
         Parent main = FXMLLoader.load(getClass().getResource("/UI/mainform.fxml"));
-        
+        Stage stage = 
     }
 }
