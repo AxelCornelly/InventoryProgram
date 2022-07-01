3e782d0d2e91dbd2b2f042979128805d9126654e
AxelCornelly <ace.cornelly@gmail.com>
2022-07-01 16:35:22 -0700

    Added new main java class to run application

--- model/Inventory.java
+++ model/Inventory.java
@@ -9,19 +9,8 @@ package model;
  */
 
 import javafx.collections.ObservableList;
-import javafx.fxml.FXMLLoader;
-import javafx.geometry.Insets;
-import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.control.TableView;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Pane;
-import java.io.IOException;
-import javafx.application.Application;
-import javafx.stage.Stage;
 
-
-public class Inventory extends Application{
+public class Inventory {
     private static ObservableList<Part> allParts;
     private static ObservableList<Product> allProducts;
 
@@ -71,19 +60,6 @@ public class Inventory extends Application{
 
     public static ObservableList<Product> getAllProducts() {
 
-    }
-
-    @Override
-    public void start(Stage appStage) throws IOException{
-        Pane mainPane = (Pane) FXMLLoader.load(getClass().getResource("/UI/mainform.fxml"));
-        Scene scene = new Scene(mainPane);
-        appStage.setScene(scene);
-        appStage.setTitle("Inventory Management System");
-        appStage.show();
-    }
-
-    public static void main(String[] args) {
-        launch(args);
     }
 
 }
