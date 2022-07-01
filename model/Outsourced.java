1c72f307581c07ca994c4e03474b8cc7dfe8227f
AxelCornelly <ace.cornelly@gmail.com>
2022-07-01 16:08:47 -0700

    changed file system and moved files around

---
+++ model/Outsourced.java
@@ -0,0 +1,27 @@
+package model;
+public class Outsourced extends Part{
+    private String companyName;
+
+    /**
+     * Constructor for Outsourced Class
+     * @param id
+     * @param name
+     * @param price
+     * @param stock
+     * @param min
+     * @param max
+     */
+    public Outsourced(int id, String name, double price, int stock, int min, int max, String companyName) {
+        super(id, name, price, stock, min, max);
+        this.companyName = companyName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+    
+}
