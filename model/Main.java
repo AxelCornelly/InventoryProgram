234aae270c453523624f9a526186bf0d0422301b
AxelCornelly <ace.cornelly@gmail.com>
2022-07-21 19:31:02 -0700

    Added Javadoc comments

--- model/Main.java
+++ model/Main.java
@@ -1,4 +1,12 @@
 package model;
+/**
+ * Main Class Main.java
+ * This is the main class for the Inventory Program.
+ */
+
+/**
+ * @author Axel Cornelly
+ */
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
@@ -8,6 +16,32 @@ import java.io.IOException;
 import javafx.stage.Stage;
 
 public class Main extends Application {
+    
+    /** 
+     * @param appStage
+     * @throws IOException
+     * 
+     * <p><b>
+     * Functionality
+     * </b></p>
+     * 
+     * This start method takes in a Stage object and throws an IOException when the parameter isn't met.
+     * This method is overriden from the parent class, Application and initializes the program's window.
+     * 
+     * <p><b>
+     * Issues
+     * </b></p>
+     * 
+     * When first using this method, I had issues with getting the Pane object to load the correct fxml file.
+     * Starting out, my program was not a Java package and was simply a collection of folders. I had to restructure
+     * my files in order to fix this.
+     * 
+     * <p><b>
+     * Improvements
+     * </b></p>
+     * 
+     * I do not think this method needs its functionality to be extended.
+     */
     @Override
     public void start(Stage appStage) throws IOException{
         Pane mainPane = (Pane) FXMLLoader.load(getClass().getResource("/view/mainform.fxml"));
@@ -17,6 +51,12 @@ public class Main extends Application {
         appStage.show();
     }
 
+    
+    /** 
+     * @param args
+     * 
+     * The location of the Javadoc files are in InventoryProgram/src
+     */
     public static void main(String[] args) {
         launch(args);
     }
